f27ebf0c294baabcdcd1fe9a9de91e21328e8665
Jose Torres <vidojam@gmail.com>
2024-07-30 09:54:22 -0400

    working on aliens

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('.results')
+const width = 15
+
+for (let i = 0; i < width * width; i++) {
+  const square = document.createElement('div')
+  grid.appendChild(square)
+}
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+console.log(squares)
+
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+
+]
+
+function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+}
+
+draw()
