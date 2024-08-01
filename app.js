b300b3d1cdb12150dbe749b914405898affbd0d7
Jose Torres <vidojam@gmail.com>
2024-08-01 09:49:20 -0400

    shooter coding

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('.results')
 const width = 15
+const aliensRemoved = []
+let currentShooterIndex = 202
 
 for (let i = 0; i < width * width; i++) {
   const square = document.createElement('div')
+  square.id = i
   grid.appendChild(square)
 }
 
@@ -15,13 +18,33 @@ const alienInvaders = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39
-
 ]
 
+// squares[alienInvaders[0]].classList.add('invader')
+
 function draw() {
+  
     for (let i = 0; i < alienInvaders.length; i++) {
+        if(!aliensRemoved.includes(i)) {
         squares[alienInvaders[i]].classList.add('invader')
+        }
     }
 }
-
 draw()
+
+squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+        case 'ArrowLeft':
+            if(currentShooterIndex % width !== 0) currentShooterIndex -= 1
+            break
+        case 'ArrowRight' :
+            if(currentShooterIndex % width < width - 1) currentShooterIndex += 1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+
+document.addEventListener('keydown', moveShooter)
